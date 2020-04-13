086120dfad8b9907a32cd87563a7ebf50243647e
Austin Powers <t3hnoobcentral@gmail.com>
2020-04-13 16:57:31 -0700

    percolation.java working, moving onto PercolationStats.java

--- Percolation (Algo1)/src/Percolation.java
+++ Percolation (Algo1)/src/Percolation.java
@@ -3,47 +3,102 @@ import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-public int[][] grid;
+	
+ private final  WeightedQuickUnionUF normalGrid;
+ private final boolean[][] grid;
+ private static int size;
+ private int openSites = 0;
+ private int topSite;
+ private int bottomSite;
+ 
 	    // creates n-by-n grid, with all sites initially blocked
 	    public Percolation(int n) {
-	    	if(n <=0 ) {
+	    	
+			if(n <=0 ) {
 	    		throw new IllegalArgumentException("n cannont be 0 or less");
 	    	}
-	    	grid = new int[n][n];
+	    	grid = new boolean[n][n];
 	    	for(int i = 0; i < n;i++) {
 	    		for(int j = 0;j<0;j++) {
-	    			grid[i][j] = -1;
+	    			grid[i][j] = false;
 	    		}
 	    	}
+	    		normalGrid = new WeightedQuickUnionUF(n*n+2);
+	    		topSite = n*n;
+	    		bottomSite= n*n+1;
+	    		size = n;
+	    		
+	    	
+	    }
+	    public static int convertInto2d(int row, int col) {
+	    	return row*size + col;
 	    }
 
 	    // opens the site (row, col) if it is not open already
 	    public void open(int row, int col) {
-	    	
+	    	if(row <size && col<size) {
+	    		if(!isOpen(row,col))
+	    		grid[row][col] = true;
+	    		openSites += 1;
+	    		if(this.isOpen(row-1,col)) {
+		    		this.normalGrid.union(convertInto2d(row, col), convertInto2d(row-1, col));
+		    		}
+	    		if(this.isOpen(row+1,col)) {
+		    		this.normalGrid.union(convertInto2d(row, col), convertInto2d(row+1, col));
+		    		}
+	    		if(this.isOpen(row,col-1)) {
+		    		this.normalGrid.union(convertInto2d(row, col), convertInto2d(row, col-1));
+		    		}
+	    		if(this.isOpen(row,col+1)) {
+		    		this.normalGrid.union(convertInto2d(row, col), convertInto2d(row, col+1));
+		    		}
+	    		if(row==0) {this.normalGrid.union(convertInto2d(row, col), topSite);}
+	    		if(row==size-1) {this.normalGrid.union(convertInto2d(row, col), bottomSite);}
+	    		
+	    		}else throw new IllegalArgumentException("Row and Col must be within the bounds");
 	    }
-
+	    
+	    		
+	    
+	    	
+	    
 	    // is the site (row, col) open?
 	    public boolean isOpen(int row, int col) {
-	    	return false;
+	    	if(row < size && row >= 0 && col >= 0 && col < size) {
+	    		return grid[row][col];	
+	    }else return false;
 	    }
+	    
 
 	    // is the site (row, col) full?
 	    public boolean isFull(int row, int col) {
-	    	return false;
+	    	if(this.normalGrid.connected(convertInto2d(row, col),topSite)) {
+	    		return true;
+	    	}else return false;
 	    }
 
 	    // returns the number of open sites
 	    public int numberOfOpenSites() {
-	    	return 0;
+	    	return openSites;
 	    }
 
 	    // does the system percolate?
 	    public  boolean percolates() {
-	    	return true;
+	    	if(this.normalGrid.connected(topSite,bottomSite)) {
+	    		return true;
+	    	}else {return false;}
 	    }
 
 	    // test client (optional)
 	    public static void main(String[] args) {
+	    	Percolation perc = new  Percolation(3);
+	    	perc.open(0, 0);
+	    	perc.open(1, 0);
+	    	perc.open(2, 0);
+	    	System.out.print(perc.normalGrid.connected(convertInto2d(0, 0),convertInto2d(2,0)));
+	    	System.out.println(perc.isFull(1, 0));
+	    	System.out.println(perc.percolates());
+	    	
 	    	
 	    }
 	}
